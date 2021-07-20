58fe2e23686197cef194add8596e95889fd71e66
sowmahe993@gmail.com <sowmahe993@gmail.com>
2021-07-20 09:58:40 +0800

    Forecast data updated

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -21,7 +21,8 @@ const forecast = (latitude, longitude, callback) => {
           body.current.temperature +
           ' degrees out. It feels like ' +
           body.current.feelslike +
-          '% degrees out.'
+          '% degrees out. The humidity is' +
+          body.current.humidity
       );
     }
   });
